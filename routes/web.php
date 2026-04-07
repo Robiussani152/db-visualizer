962be7d01bc6c09ea3c5ad13042af79ef64574b1
Robius Sani <sani15-257@diu.edu.bd>
2026-04-07 12:15:25 +0600

    feat(config): make package routes and settings env-configurable

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
-use Naimul\DbVisualizer\Http\Controllers\VisualizerController;
 
-Route::prefix('dbv')->group(function () {
-    Route::get('/', [VisualizerController::class, 'index']);
-    Route::get('/data', [VisualizerController::class, 'data']);
-    Route::get('/detail/{model}', [VisualizerController::class, 'detail']);
-});
+Route::get('/', 'VisualizerController@index');
+Route::get('/data', 'VisualizerController@data');
+Route::get('/detail/{model}', 'VisualizerController@detail');
--- src/DbVisualizerServiceProvider.php
+++ src/DbVisualizerServiceProvider.php
@@ -1,27 +1,75 @@
 <?php
+
 namespace Naimul\DbVisualizer;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class DbVisualizerServiceProvider extends ServiceProvider
 {
+    public function boot(): void
+    {
+        $this->registerPublishing();
+
+        if (!config('db-visualizer.enabled')) {
+            return;
+        }
+
+        Route::middlewareGroup('db-visualizer', [
+            ...config('db-visualizer.middleware', ['web']),
+        ]);
+
+        $this->registerRoutes();
+        $this->registerResources();
+    }
+
     public function register(): void
     {
         $this->mergeConfigFrom(__DIR__.'/../config/db-visualizer.php', 'db-visualizer');
     }
 
-    public function boot(): void
+    /**
+     * Register the package's publishable resources.
+     *
+     * @return void
+     */
+    protected function registerPublishing()
     {
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/db-visualizer.php' => config_path('db-visualizer.php'),
+            ], 'dbv-config');
 
-        $this->publishes([
-            __DIR__.'/../config/db-visualizer.php' => config_path('db-visualizer.php'),
-        ], 'dbv-config');
-        
+            $this->publishes([
+                __DIR__.'/Resources/views' => resource_path('views/vendor/dbv'),
+            ], 'dbv-views');
+        }
+    }
+
+    /**
+     * Register the package routes.
+     *
+     * @return void
+     */
+    protected function registerRoutes()
+    {
+        Route::group([
+            'domain' => config('db-visualizer.domain', null),
+            'namespace' => 'Naimul\DbVisualizer\Http\Controllers',
+            'prefix' => config('db-visualizer.path'),
+            'middleware' => 'db-visualizer',
+        ], function () {
+            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        });
+    }
+
+    /**
+     * Register the DB Visualizer resources.
+     *
+     * @return void
+     */
+    protected function registerResources()
+    {
         $this->loadViewsFrom(__DIR__.'/../Resources/views', 'dbv');
-
-        $this->publishes([
-            __DIR__.'/Resources/views' => resource_path('views/vendor/dbv'),
-        ], 'dbv-views');
     }
 }
